26d897b315105d3dbe29a2b573bc771371764c8f
notsamuelwang <notsamuelwang@gmail.com>
2023-01-20 22:18:47 -0500

    navbar skeleton

---
+++ client/src/components/pages/Game.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { RouteComponentProps } from "@reach/router";
+
+const Game = (props) => {
+  return (
+    <div>
+      <h1>The page you are looking for is in another castle!</h1>
+    </div>
+  );
+};
+
+export default Game;
