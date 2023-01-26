a46cfc01f753a9d6930f6bf99da9b773917aba0b
notsamuelwang <notsamuelwang@gmail.com>
2023-01-26 16:04:56 -0500

    canvas

--- client/src/client-socket.ts
+++ client/src/client-socket.ts
@@ -1,3 +1,7 @@
 import socketIOClient from "socket.io-client";
 const endpoint = `${window.location.hostname}:${window.location.port}`;
 export const socket = socketIOClient.io(endpoint);
+
+//export const move = (dir) => {
+//  socket.emit("move", dir);
+//};
--- client/src/components/modules/GameCanvas.tsx
+++ client/src/components/modules/GameCanvas.tsx
@@ -3,6 +3,33 @@ import { socket } from "../../client-socket";
 import { get, post } from "../../utilities";
 import { drawCanvas } from "../../canvasManager";
 
+//utils copy and pasted
+type StateData = {
+  food: Food[];
+}
+
+type Position = {
+x: number;
+y: number;
+};
+
+type Food = {
+position: Position;
+radius: number;
+color: string;
+};
+
+type Player = {
+  position: Position;
+  radius: number;
+  color: string;
+}
+
+type Coord = {
+drawX: number;
+drawY: number;
+};
+
 const GameCanvas = (props) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
@@ -14,8 +41,22 @@ const GameCanvas = (props) => {
       context.fillRect(0, 0, context.canvas.width, context.canvas.height);
     }
   }, []);
-
-  return <canvas ref={canvasRef} {...props} />;
+  const gameState: {food: Food[], players: Player[]} = {
+    food: [],
+    players: [],
+  }
+  gameState.players.push({
+    position: {x: 400, y: 400,},
+    radius: 10,
+    color: "red",
+  });
+  useEffect(() => {
+    drawCanvas(gameState);
+  }
+  )
+  return (
+    <canvas id="game-canvas" width="500" height="500"/>//ref={canvasRef} {...props} />
+  );
 };
 
 export default GameCanvas;
