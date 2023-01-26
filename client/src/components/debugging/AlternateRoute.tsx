f36018f512cb022232dd2484bc7c19b22380832a
Eric Yang <ericyang800@gmail.com>
2023-01-25 22:06:55 -0500

    added debug page

---
+++ client/src/components/debugging/AlternateRoute.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type AlternateRouteProps = {};
+
+const AlternateRoute = (props: AlternateRouteProps) => {
+  return (
+    <div>
+      <h1>DEBUG TEXT</h1>
+    </div>
+  );
+};
+
+export default AlternateRoute;
